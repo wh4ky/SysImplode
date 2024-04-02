d5c2ebeffdd9ac147ea92920ca0fb8dd558f548b
wh4ky <damian.whacky@proton.me>
2024-04-02 17:57:52 +0200

    Minor notation changes

--- src/SysImplode.cpp
+++ src/SysImplode.cpp
@@ -6,7 +6,7 @@
 #include <random>
 #include <limits>
 
-// Mersenne twister algorithm
+// Mersenne-Twister algorithm
 std::mt19937 mt;
 const int MAX_NUM = std::numeric_limits<int>::max();
 
@@ -16,16 +16,16 @@ void spawnThreads(int, int);
 int main() {
     int init, thrd, times;
 
-    std::cout << "seed value (0 for semi-random seed): ";
+    std::cout << "Seed value to use (0 for a semi-random seed): ";
     std::cin >> init;
     if (init == 0) {init = time(nullptr);}
     mt.seed(init);
 
-    std::cout << "How many times to run algorithm (0 for maximum interger): ";
+    std::cout << "How many times to run the algorithm (0 for the maximum interger): ";
     std::cin >> times;
     if (times == 0) {times = MAX_NUM;}
 
-    std::cout << "ammount of threads (0 for maximum interger): ";
+    std::cout << "Amount of threads to create (0 for the maximum interger): ";
     std::cin >> thrd;
     if (thrd == 0) {thrd = MAX_NUM;}
     spawnThreads(thrd, times);
