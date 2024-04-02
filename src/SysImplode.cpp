826e2e7796c962b52ed53fead237738832976eab
wh4ky <damian.whacky@proton.me>
2024-04-02 17:30:38 +0200

    more customizability

--- src/SysImplode.cpp
+++ src/SysImplode.cpp
@@ -4,40 +4,49 @@
 #include <string>
 #include <vector>
 #include <random>
+#include <limits>
 
 // Mersenne twister algorithm
 std::mt19937 mt;
+const int MAX_NUM = std::numeric_limits<int>::max();
 
-void RandomNum();
-void spawnThreads(int);
+void RandomNum(int);
+void spawnThreads(int, int);
 
 int main() {
-    int init, thrd;
+    int init, thrd, times;
 
     std::cout << "seed value (0 for semi-random seed): ";
     std::cin >> init;
     if (init == 0) {init = time(nullptr);}
     mt.seed(init);
 
-    std::cout << "ammount of threads: ";
+    std::cout << "How many times to run algorithm (0 for maximum interger): ";
+    std::cin >> times;
+    if (times == 0) {times = MAX_NUM;}
+
+    std::cout << "ammount of threads (0 for maximum interger): ";
     std::cin >> thrd;
-    spawnThreads(thrd);
+    if (thrd == 0) {thrd = MAX_NUM;}
+    spawnThreads(thrd, times);
 }
 
 
 // Functions
-void RandomNum() {
-    std::cout << mt();
-}
-
-void spawnThreads(int n) {
-    std::vector<std::thread> threads(n);
+void spawnThreads(int thrd, int times) {
+    std::vector<std::thread> threads(thrd);
     
-    for (int i = 0; i < n; i++) {
-        threads[i] = std::thread(RandomNum);
+    for (int i = 0; i < thrd; i++) {
+        threads[i] = std::thread(RandomNum, times);
     }
 
     for (auto& th : threads) {
         th.join();
+    }
+}
+
+void RandomNum(int times) {
+    for (int i = 0; i < times; i++) {
+        std::cout << mt();
     }
 }
